0c1f62922d73f477455ee2692c08584d02cd45fa
Alex <84599917+GyverLibs@users.noreply.github.com>
2022-02-15 09:33:36 +0300

    upd

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![Foo](https://img.shields.io/badge/Version-1.1-brightgreen.svg?style=flat-square)](#versions)
+[![Foo](https://img.shields.io/badge/Version-1.2-brightgreen.svg?style=flat-square)](#versions)
 [![Foo](https://img.shields.io/badge/Website-AlexGyver.ru-blue.svg?style=flat-square)](https://alexgyver.ru/)
 [![Foo](https://img.shields.io/badge/%E2%82%BD$%E2%82%AC%20%D0%9D%D0%B0%20%D0%BF%D0%B8%D0%B2%D0%BE-%D1%81%20%D1%80%D1%8B%D0%B1%D0%BA%D0%BE%D0%B9-orange.svg?style=flat-square)](https://alexgyver.ru/support_alex/)
 
@@ -72,6 +72,7 @@ void loop() {
 ## Версии
 - v1.0
 - v1.1 - небольшая оптимизация, повышение точности
+- v1.2 - оптимизация, поддержка 100к термисторов
 
 <a id="feedback"></a>
 ## Баги и обратная связь
--- src/GyverNTC.h
+++ src/GyverNTC.h
@@ -14,17 +14,18 @@
     Версии:
     v1.0 - релиз
     v1.1 - небольшая оптимизация, повышение точности
+    v1.2 - оптимизация, поддержка 100к термисторов
 */
 
-#ifndef GyverNTC_h
-#define GyverNTC_h
+#ifndef _GyverNTC_h
+#define _GyverNTC_h
 #include <Arduino.h>
 #define _T_SAMPLE_AVERAGE 20   // количество чтений для усреднения
 
 class GyverNTC {
 public:
-    GyverNTC(byte pin, int resistance, int beta, int tempBase = 25, int resistBase = 10000) :
-    _pin(pin), _resistance(resistance), _beta(beta), _tempBase(tempBase), _resistBase(resistBase) {}
+    GyverNTC(uint8_t pin, uint32_t resistance, uint16_t beta, uint8_t tempBase = 25, uint32_t base = 10000) :
+    _pin(pin), _beta(beta), _tempBase(tempBase), _baseDivRes((float)base / resistance) {}
     
     // прочитать температуру с пина
     float getTemp() {
@@ -34,22 +35,21 @@ public:
     // прочитать усреднённую температуру с пина
     float getTempAverage() {
         uint16_t aver = 0;
-        for (byte i = 0; i < _T_SAMPLE_AVERAGE; i++) aver += analogRead(_pin);
+        for (uint8_t i = 0; i < _T_SAMPLE_AVERAGE; i++) aver += analogRead(_pin);
         return computeTemp((float)aver / _T_SAMPLE_AVERAGE);
     }
     
     // получить температуру из сигнала АЦП (10 бит, float)
     float computeTemp(float analog) {
-        analog = (float)_resistBase / _resistance / (1023.0f / analog - 1.0);
-        analog = (log(analog) / _beta) + 1.0f / (_tempBase + 273.15f);
-        return (1.0f / analog - 273.15);
+        analog = _baseDivRes / (1023.0f / analog - 1.0);
+        analog = (log(analog) / _beta) + 1.0 / (_tempBase + 273.15);
+        return (1.0 / analog - 273.15);
     }
     
 private:    
-    const byte _pin = 0;
-    const int _beta = 0;
-    const int _tempBase = 25;
-    const int _resistance = 10000;
-    const int _resistBase = 10000;
+    const uint8_t _pin = 0;
+    const uint16_t _beta = 0;
+    const uint8_t _tempBase = 25;
+    const float _baseDivRes;
 };
 #endif
