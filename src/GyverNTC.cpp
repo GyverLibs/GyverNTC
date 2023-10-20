0a2dd5b636e7dd1e5270fab55a1772b770e13548
AlexGyver <alexgyvershow@ya.ru>
2023-10-20 19:42:22 +0300

    Update GyverNTC.cpp

--- src/GyverNTC.cpp
+++ src/GyverNTC.cpp
@@ -1,13 +1,13 @@
 #include "GyverNTC.h"
 
 // сигнал АЦП, (R резистора / R термистора), B термистора, t термистора, разрешение АЦП
-float NTC_compute(float analog, float baseDiv, uint16_t B, uint8_t t = 25, uint8_t res = 10) {
+float NTC_compute(float analog, float baseDiv, uint16_t B, uint8_t t, uint8_t res) {
     analog = baseDiv / ((float)((1 << res) - 1) / analog - 1.0f);
     analog = (log(analog) / B) + 1.0f / (t + 273.15f);
     return (1.0f / analog - 273.15f);
 }
 
 // сигнал АЦП, R резистора, B термистора, t термистора, R термистора, разрешение АЦП
-float NTC_compute(float analog, uint32_t R, uint16_t B, uint8_t t = 25, uint32_t Rt = 10000, uint8_t res = 10) {
+float NTC_compute(float analog, uint32_t R, uint16_t B, uint8_t t, uint32_t Rt, uint8_t res) {
     return NTC_compute(analog, (float)R / Rt, B, t, res);
 }
